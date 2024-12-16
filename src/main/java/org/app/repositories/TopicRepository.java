433ef015f8b06f3d98a1a0098c54921dd4c538ee
DanilZam <danil29041@gmail.com>
2024-12-16 20:02:05 +0300

    Made a skeleton of the TopicRepository class

--- src/main/java/org/app/repositories/TopicRepository.java
+++ src/main/java/org/app/repositories/TopicRepository.java
@@ -1,4 +1,29 @@
 package org.app.repositories;
 
+import org.app.entities.Topic;
+import org.app.entities.Topic;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TopicRepository {
+    private List<Topic> topicCache = new ArrayList<>();
+    private  final String filePath = "D:/JavaProject/RSiOT_Forum/src/main/resources/topics.txt";
+
+    public void loadTopicData(){
+
+    }
+
+    public void saveDataChanges(){
+
+    }
+
+    public List<Topic> getAllTopics(){
+        return new ArrayList<>(topicCache);
+    }
+
+    public void addTopic(Topic topic){
+        topicCache.add(topic);
+        saveDataChanges();
+    }
 }
