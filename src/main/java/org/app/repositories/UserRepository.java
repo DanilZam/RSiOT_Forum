5d84efd81903561b867229f19b4500417eaf1acc
DanilZam <danil29041@gmail.com>
2024-12-16 20:02:15 +0300

    Made a skeleton of the UserRepository class

--- src/main/java/org/app/repositories/UserRepository.java
+++ src/main/java/org/app/repositories/UserRepository.java
@@ -1,4 +1,28 @@
 package org.app.repositories;
 
+import org.app.entities.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRepository {
+    private List<User> userCache = new ArrayList<>();
+    private  final String filePath = "D:/JavaProject/RSiOT_Forum/src/main/resources/users.txt";
+
+    public void loadUserData(){
+
+    }
+
+    public void saveDataChanges(){
+
+    }
+
+    public List<User> getAllUsers(){
+        return new ArrayList<>(userCache);
+    }
+
+    public void addUser(User user){
+        userCache.add(user);
+        saveDataChanges();
+    }
 }
