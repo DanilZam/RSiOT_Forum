9442492c5608486ef3e494788985ddad5ab8f21f
DanilZam <danil29041@gmail.com>
2024-12-16 19:59:04 +0300

    Made a skeleton of the MessageRepository class

--- src/main/java/org/app/repositories/MessageRepository.java
+++ src/main/java/org/app/repositories/MessageRepository.java
@@ -1,4 +1,30 @@
 package org.app.repositories;
 
+import org.app.entities.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MessageRepository {
+
+    private List<Message> messageCache = new ArrayList<>();
+    private  final String filePath = "D:/JavaProject/RSiOT_Forum/src/main/resources/message.txt";
+
+    public void loadMessageData(){
+
+    }
+
+    public void saveDataChanges(){
+
+    }
+
+    public List<Message> getAllMessages(){
+        return new ArrayList<>(messageCache);
+    }
+
+    public void addMessage(Message message){
+        messageCache.add(message);
+        saveDataChanges();
+    }
+
 }
